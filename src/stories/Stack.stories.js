723a0a9b3f031980a8c91c212471185ad194a126
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-15 14:52:00 +0300

    add stack template

---
+++ src/stories/Stack.stories.js
@@ -0,0 +1,28 @@
+import Stack from '../components/Stack';
+
+export default {
+    title: 'Components/Stack',
+    component: Stack,
+    argTypes: {
+        numberOfChildren: { type: 'number', defaultValue: 4 },
+    },
+};
+
+const Template = (numberOfChildren, ...args) => (
+    <Stack {...args}>
+        {[...Array(numberOfChildren).keys()].map((n) => (
+            <div
+                style={{
+                    width: '50px',
+                    height: '50px',
+                    backgroundColor: 'red',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                {n + 1}
+            </div>
+        ))}
+    </Stack>
+);
