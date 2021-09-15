e70a750b6f4c5f5e714f1bdfd44435f3b5ea07a7
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-15 14:56:25 +0300

    add 5 stack templates

--- src/stories/Stack.stories.js
+++ src/stories/Stack.stories.js
@@ -8,7 +8,7 @@ export default {
     },
 };
 
-const Template = (numberOfChildren, ...args) => (
+const Template = ({ numberOfChildren, ...args }) => (
     <Stack {...args}>
         {[...Array(numberOfChildren).keys()].map((n) => (
             <div
@@ -26,3 +26,40 @@ const Template = (numberOfChildren, ...args) => (
         ))}
     </Stack>
 );
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+    direction: 'row',
+    spacing: 2,
+    wrap: false,
+};
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+    direction: 'column',
+    spacing: 2,
+    wrap: false,
+};
+
+export const NoSpacing = Template.bind({});
+NoSpacing.args = {
+    direction: 'row',
+    spacing: 0,
+    wrap: false,
+};
+
+export const WrapOverflow = Template.bind({});
+WrapOverflow.args = {
+    numberOfChildren: 40,
+    direction: 'row',
+    spacing: 2,
+    wrap: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+    numberOfChildren: 0,
+    direction: 'row',
+    spacing: 2,
+    wrap: false,
+};
