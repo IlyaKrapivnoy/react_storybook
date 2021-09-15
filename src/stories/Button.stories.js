958cbfca3d1369439c4357f7d070980454ede437
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-15 14:23:25 +0300

    add btn templates

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -5,4 +5,25 @@ export default {
     component: Button,
 };
 
-export const Red = () => <Button label='Press me' backgroundColor='red' />;
+const Template = (args) => <Button {...args} />;
+
+export const Red = Template.bind();
+Red.args = {
+    backgroundColor: 'red',
+    label: 'Press me',
+    size: 'md',
+};
+
+export const Green = Template.bind();
+Green.args = {
+    backgroundColor: 'green',
+    label: 'Press me',
+    size: 'md',
+};
+
+export const Gray = Template.bind();
+Gray.args = {
+    backgroundColor: 'gray',
+    label: 'Press me',
+    size: 'md',
+};
